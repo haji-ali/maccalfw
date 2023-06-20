84f3c27a6d7032a42bba865b56d3c364ea333c04
Al Haji-Ali <a.hajiali@hw.ac.uk>
2023-06-20 17:04:49 +0100

    Add documentation

--- maccalfw.el
+++ maccalfw.el
@@ -1,8 +1,12 @@
-;;; maccalfw.el --- calendar view for Mac Calendars -*- lexical-binding: t; -*-
+;;; maccalfw.el --- Calendar view for Mac Calendars -*- lexical-binding: t; -*-
 
-;;Copyright (C) 2011  SAKURAI Masashi
+;; Copyright (C) 2023 Al Haji-Ali
 
-;; Author: SAKURAI Masashi <m.sakurai at kiwanami.net>
+;; Author: Al Haji-Ali <abdo.haji.ali at gmail.com>
+;; Created: 2023
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1"))
+;; Homepage: https://github.com/haji-ali/maccalfw
 ;; Keywords: calendar
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -20,8 +24,8 @@
 
 ;;; Commentary:
 
-;; A bridge from Mac Calendar to calfw.
-;; The API and interfaces have not been confirmed yet.
+;; Bridge from Mac Calendar to calfw. The API and interfaces have not been
+;; confirmed yet.
 
 ;;; Installation:
 
@@ -35,12 +39,18 @@
 (require 'calfw)
 (require 'calfw-blocks)
 
-(defun maccalfw--decode-date (date)
-  (list (decoded-time-month date)
-        (decoded-time-day date)
-        (decoded-time-year date)))
+(defun maccalfw--decode-date (time)
+  "Return a calendar date from encoded TIME.
+The return value is (month day year)."
+  (list (decoded-time-month time)
+        (decoded-time-day time)
+        (decoded-time-year time)))
 
 (defun maccalfw--encode-date (date &optional end-of-day)
+  "Encode a calendar DATE.
+DATE is of the format (month day year). If END-OF-DAY is nil, the
+time is midnight, otherwise it is a second before midnight of the
+next day."
   (encode-time (append
                 (if end-of-day
                     (list 59 59 23)
@@ -49,12 +59,15 @@
                      (nth 0 date)
                       (nth 2 date)))))
 
-(defun maccalfw--decode-time (date)
-  (list (decoded-time-hour date)
-        (decoded-time-minute date)))
+(defun maccalfw--decode-time (time)
+  "Return a calendar time from encoded TIME."
+  (list (decoded-time-hour TIME)
+        (decoded-time-minute TIME)))
 
 
-(defun maccalfw-convert-event (event)
+(defun maccalfw--convert-event (event)
+  "Convert an EVENT to a calfw event.
+The event is returned `maccalfw-fetch-events'."
   (let ((start (decode-time (plist-get event :start)))
         (end (decode-time (plist-get event :end)))
         (all-day-p (plist-get event :all-day-p)))
@@ -71,6 +84,7 @@
      :description (plist-get event :summary))))
 
 (defun maccalfw-convert-to-calfw (events-list)
+  "Convert an EVENTS-LIST to calfw events."
   (cl-loop for e in events-list
         for event = (maccalfw-convert-event e)
         if event
@@ -84,7 +98,10 @@
           (message "Cannot handle this event, tag: %s" e))
         finally (return `((periods ,periods) ,@contents))))
 
-(defun maccalfw-to-calendar (cal-id begin end)
+(defun maccalfw-get-calendar (cal-id begin end)
+  "Return all calendar event corresponding CAL-ID.
+BEING and END are dates with the format (month day year). The
+events between BEGIN and END are returned."
   (cl-loop for event in
         (maccalfw-convert-to-calfw
             (maccalfw-fetch-events cal-id
@@ -101,6 +118,10 @@
         collect event))
 
 (defun maccalfw-create-source (name cal-id color)
+  "Create a cfw:source out of a calendar.
+CAL-ID is the ID of the calendar and get be obtained with
+`maccalfw-get-calendars'. The calendar's NAME and COLOR are set
+accordingly."
   (make-cfw:source
    :name name
    :color color
@@ -111,19 +132,25 @@
            (maccalfw-to-calendar cal-id begin end))))
 
 (defun maccalfw-get-calendars-by-name (names)
+  "Return the calendar IDs with NAMES."
   (--filter
    (member (plist-get it :title) names)
    (maccalfw-get-calendars)))
 
 (defun maccalfw--load-module (&optional force)
+  "Load an compile dynamic module for maccalfw.
+If FORCE is nil, then the module is not compiled nor re-loaded if
+already loaded. \\='compile forces recompilation before
+re-loading and \\='compile-only forces recompilation without
+loading."
   (unless (and (not force) (fboundp #'maccalfw-get-calendars))
     (unless module-file-suffix
       (error "maccalfw: Dynamic modules are not supported"))
     (let* ((mod-name (file-name-with-extension
-                      "maccalfw"
+                      "libmaccalfw"
                       module-file-suffix))
            (mod-file (locate-library mod-name t)))
-      (unless (and mod-file (not (eq force 'compile)))
+      (unless (and mod-file (not (member force '(compile compile-only))))
         (let* ((swift (or (getenv "SWIFTC")
                           (executable-find "swiftc")
                           (error "maccalfw: No swift compiler found")))
@@ -131,6 +158,7 @@
                                    (feature-file 'maccalfw)))
                (command
                 `(,swift "-Xcc" "-fmodule-map-file=src/module.modulemap"
+                         "-I/opt/homebrew/include/"
                          "src/EmacsUtil.swift"
                          "src/MacCalfw.swift"
                          "-O" "-emit-library"
@@ -151,11 +179,13 @@
                   (pop-to-buffer (current-buffer))
                   (error msg)))))
           (setq mod-file (expand-file-name mod-name))))
-      (module-load mod-file))))
+      (unless (eq force 'compile-only)
+        (module-load mod-file)))))
 
-(defun maccalfw (&optional calendars)
-  "Simple calendar interface. This command displays all CALENDARS
-obtained using `maccalfw-get-calendars' or all of them if it is 'all."
+(defun maccalfw-open (&optional calendars)
+  "Open a calfw calendar with CALENDARS from Apple's Calendar.
+This command displays any CALENDARS obtained using
+`maccalfw-get-calendars' or all of them if it is \\='all."
   (interactive (list 'all))
   (maccalfw--load-module)
   (when (eq calendars 'all)
@@ -171,9 +201,6 @@ obtained using `maccalfw-get-calendars' or all of them if it is 'all."
                                           (plist-get x :color)))
                 calendars))))
       (switch-to-buffer (cfw:cp-get-buffer cp)))))
-
-;; (cfw:open-maccalfw-calendar
-;;  (cfw:get-calendars-by-name '("iCloud Family")))
 
 (provide 'maccalfw)
 ;;; maccalfw.el ends here
